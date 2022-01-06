cf81a41645a464404fdfb1e6d6b32c73cc1545c5
Maqsud <maqsudtolipov9@gmail.com>
2022-01-06 23:14:25 +0500

    Add javascript alert

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// show alert after 3 seconds
+setTimeout(function() {
+  alert('Hello my friend! Sorry for the alert, but I have to tell you something that my website is still under construction. I will be back soon! 😉👌' );
+}, 3000);
