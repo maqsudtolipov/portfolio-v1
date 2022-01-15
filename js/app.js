e8d404373ba569f961cefcba9f902f9f1daaa5ae
Maqsud <maqsudtolipov9@gmail.com>
2022-01-15 22:06:23 +0500

    Make navigation work for tablets and below

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@
 // setTimeout(function() {
 //   alert('Hello my friend! Sorry for the alert, but I have to tell you something that my website is still under construction. I will be back soon! 😉👌' );
 // }, 3000);
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
