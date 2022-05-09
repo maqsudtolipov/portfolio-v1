b0854b3b004cb9cc795df956b7e10c967bacc037
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-05-09 06:27:20 +0500

    Handle GET requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "📢 Hello from the server" });
+app.get("/api/v1/projects", (req, res) => {
+  res.json({
+    success: true,
+    message: "Projects fetched successfully",
+    projects: [],
+  });
 });
 
 const port = 1000;
