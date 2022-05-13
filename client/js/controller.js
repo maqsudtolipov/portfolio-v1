203fea3a1f63e4ee58209e572ef71d45ad673d3c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-05-13 23:43:20 +0500

    Render project

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,9 +1,11 @@
 // https://maqsud-me.herokuapp.com/api/v1
 
 ////////////////////////////////////////
+const projectsContiner = document.querySelector("#projects");
 
 const showProject = async function () {
   try {
+    // Loading project
     const res = await fetch(
       "https://maqsud-me.herokuapp.com/api/v1/projects/627ea2f9176c6b5edfa5bbbc"
     );
@@ -13,6 +15,20 @@ const showProject = async function () {
 
     let project = data.data.project;
     console.log(project);
+
+    // Rendering project
+    const markup = `
+      <div class="work">
+        <div class="work__img">
+          <a href="#" target="_blank">
+            <img src="${project.image}" alt="My recent work"
+          /></a>
+        </div>
+        <p class="work__title">${project.name}</p>
+        <p class="work__info">${project.description}</p>
+      </div>`;
+    projectsContiner.innerHTML = "";
+    projectsContiner.insertAdjacentHTML("beforeend", markup);
   } catch (err) {
     console.log(err);
   }
