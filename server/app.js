ccf23cc4dd45158d6239ad8a73af0b7e15e6fe96
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-05-13 21:36:40 +0500

    Fix server error

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
-
 const express = require("express");
-const projectRouter = require("./routes/projectRoutes");
 const morgan = require("morgan");
+const projectRouter = require("./routes/projectRoutes");
 
 const app = express();
 
