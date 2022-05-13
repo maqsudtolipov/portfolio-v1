d796f48bd9e687e0cb00dfd91eebfd1f1bf88f1c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-05-13 22:42:39 +0500

    Install cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
+const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 const projectRouter = require("./routes/projectRoutes");
 
 const app = express();
 
-
-
 // Middlewares
+app.use(cors());
+app.options("*", cors());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
